36e647e020ce7a8c4dd4bd562ead8205e66fe34f
Michael Boroski <mjboroski@gmail.com>
2018-07-09 20:23:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ class Passenger{
   }
 
   trips(){
-    return store.trips.filter(function(){this.id===passenger.id});
+    return store.trips.filter(function(){this.id === passengerId});
   }
 
   drivers(){
