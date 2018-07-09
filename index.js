ae58c8ff85c5edc479ee843309d00717797da438
Michael Boroski <mjboroski@gmail.com>
2018-07-09 20:28:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ let driverId = 0;
 let passengerId = 0;
 let tripId = 0;
 
-class Driver{
-  constructor(name){
+class Driver {
+  constructor(name) {
     this.name = name;
     this.id = driverId++;
     store.drivers.push(this);
   }
-
   trips() {
     return store.trips.filter(trip => {
       return trip.driverId == this.id;
     });
   }
-
-  passengers(){
+  passengers() {
     return this.trips().map(trip => {
       return trip.passenger();
     });
