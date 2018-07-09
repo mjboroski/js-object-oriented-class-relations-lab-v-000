e83778b434c5056a3b703a117e78fdb10cc44c67
Michael Boroski <mjboroski@gmail.com>
2018-07-09 20:28:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,44 +11,53 @@ class Driver{
     store.drivers.push(this);
   }
 
-  trips(){
-    return store.trips.filter(function(){this.id === driverId});
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
   }
 
   passengers(){
-
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
   }
 }
 
-class Passenger{
-  constructor(name){
+class Passenger {
+  constructor(name) {
     this.name = name;
     this.id = passengerId++;
-    store.passengers.push(this);;
+    store.passengers.push(this);
   }
 
-  trips(){
-    return store.trips.filter(function(){this.id === passengerId});
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    });
   }
-
-  drivers(){
-
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
   }
 }
 
-class Trip{
-  constructor(driver, passenger){
-    this.id = tripId++;
+class Trip {
+  constructor(driver, passenger) {
     this.driverId = driver.id;
     this.passengerId = passenger.id;
+    this.id = tripId++;
     store.trips.push(this);
   }
-
-  driver(){
-
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
   }
-
-  passenger(){
-
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
   }
 }
