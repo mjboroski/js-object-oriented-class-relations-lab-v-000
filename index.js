26e88ee7aa0960281fd02308a2dee2eb21b65fee
Michael Boroski <mjboroski@gmail.com>
2018-07-09 20:14:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+class Driver{
+  constructor(){
+    
+  }
+
+  newDriver(){
+
+  }
+
+  trips(){
+    
+  }
+
+  passengers(){
+    
+  }
+}
+
+class Passenger{
+  constructor(){
+    
+  }
+  
+  newPassenger(){
+
+  }
+
+  trips(){
+    
+  }
+
+  drivers(){
+    
+  }
+}
+
+class Trip{
+  constructor(){
+    
+  }
+  
+}
